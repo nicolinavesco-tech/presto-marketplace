ac92cba1fc81c1cd495cddbeb8a5d1747963b238
Elisabetta Petruzzi Merli <elisabetta.petruzzimerli@gmail.com>
2026-02-05 15:47:15 +0100

    ultimi dettagli filtro categoria

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -7,7 +7,7 @@
         <h6 class="card-subtitle text-body-secondary">{{$article->price}}$</h6>
         <div class="d-flex justify-content-evenly align-items-center mt-5">
             <a href="{{route('article_show', compact('article'))}}" class="btn btn-danger">Dettaglio</a>
-            <a href="#" class="btn btn-outline-info">Categoria</a>
+            <a href="{{route("byCategory", ["category"=>$article->category])}}" class="btn btn-outline-info">{{$article->category->name}}</a>
         </div>
 
     </div>
