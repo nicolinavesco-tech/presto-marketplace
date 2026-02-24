00fe74d3645b91f830347760ab2ce4ef411f3d68
Nicolina <nicolina.vesco@gmail.com>
2026-02-24 22:16:59 +0100

    aggiunta nel CategoriesSeeder firstOrCreate per non far ripetere il seed

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -26,7 +26,7 @@ class CategoriesSeeder extends Seeder
     public function run(): void
     {
         foreach ($this->categories as $category) {
-            Category::create([
+            Category::firstOrCreate([
                 'name' => $category
             ]);
         }
