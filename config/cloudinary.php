6ea9a8687ca2a83744cb6ea87886806643d634e4
Nicolina <nicolina.vesco@gmail.com>
2026-03-02 20:58:48 +0100

    cambiato cloudinary.php

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,25 +1,19 @@
 <?php
 
+$cloudUrl = env('CLOUDINARY_URL');
+
+if (!$cloudUrl) {
+    $cloud = env('CLOUDINARY_CLOUD_NAME');
+    $key   = env('CLOUDINARY_API_KEY');
+    $sec   = env('CLOUDINARY_API_SECRET');
+
+    if ($cloud && $key && $sec) {
+        $cloudUrl = "cloudinary://{$key}:{$sec}@{$cloud}";
+    }
+}
+
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Cloudinary URL
-    |--------------------------------------------------------------------------
-    | Format: cloudinary://API_KEY:API_SECRET@CLOUD_NAME
-    */
-    'cloud_url' => env('CLOUDINARY_URL'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Upload preset (solo se fai upload unsigned dal frontend)
-    |--------------------------------------------------------------------------
-    */
+    'cloud_url' => $cloudUrl,
     'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Altre opzioni
-    |--------------------------------------------------------------------------
-    */
     'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
 ];
